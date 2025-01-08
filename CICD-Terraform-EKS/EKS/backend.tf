df0fa9d7dd8da9a9b4d2284fc277c5008b929a22
tusharaws072 <tusharaws072@gmail.com>
2025-01-08 12:06:43 +0530

    Update backend.tf

--- CICD-Terraform-EKS/EKS/backend.tf
+++ CICD-Terraform-EKS/EKS/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "mytodoappbucket"
+    bucket = "phoenix12345"
     key    = "eks/terraform.tfstate"
-    region = "us-east-2"
+    region = "ap-southeast-1"
   }
 }
